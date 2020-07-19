d773af16e4772beaa830669b63ead4df943445d7
KOTIKONE\Merja <merja.lipponen@elisanet.fi>
2020-07-19 19:09:08 +0300

    Harjotustyö Merja L.

---
+++ app/src/main/java/com/example/vakuutuslaskin_nro2/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.vakuutuslaskin_nro2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.editTextTextUsername);
+        Password = (EditText) findViewById(R.id.editTextTextPassword);
+        Login = (Button) findViewById(R.id.Login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+                finish();
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword) {
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))) {
+            Intent intent = new Intent(getApplicationContext(), Activity2.class);
+            startActivity(intent);
+        } else {
+            counter--;
+            //TODO tähän
+            if (counter == 0) {
+                // TODO tähän joku teksti
+                Login.setEnabled(false);
+            }
+        }
+    }
+}
+
+ //           public void exit() { // TODO lisää tämä sivulle 2 jotta sieltä voi palata etusivulle?
+ //               Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+ //               intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+ //               intent.putExtra("EXIT", true);
+ //               finish();
+ //           }
+
